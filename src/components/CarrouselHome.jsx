c861be79785b63315f08a96dbe4e86b1fe5dbff0
Android Studio <devandroid@ort>
2025-07-08 10:33:05 -0300

    fin de la jornada

--- src/components/CarrouselHome.jsx
+++ src/components/CarrouselHome.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import './Carrousel.css';
-
+//https://react-bootstrap.netlify.app/docs/components/carousel   (Controlled)
 function Carrousel() {
   const [index, setIndex] = useState(0);
 
@@ -15,31 +15,26 @@ function Carrousel() {
         <Carousel.Item>
             <img src ="./src/assets/Home1.jpg" />
             <Carousel.Caption>
-            <h3>First slide label</h3>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img src ="./src/assets/Home2.jpg" />
             <Carousel.Caption>
-            <h3>Second slide label</h3>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img src ="./src/assets/Home3.jpg" />
             <Carousel.Caption>
-            <h3>Third slide label</h3>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img src ="./src/assets/Home4.jpg" />
             <Carousel.Caption>
-            <h3>Third slide label</h3>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img src ="./src/assets/Home5.jpg" />
             <Carousel.Caption>
-            <h3>Third slide label</h3>
             </Carousel.Caption>
         </Carousel.Item>
         </Carousel>
