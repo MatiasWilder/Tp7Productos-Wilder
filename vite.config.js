9e675640df1f77f7222582c0c454ade2fde4df81
Matias Wilder <48178322@est.ort.edu.ar>
2025-07-09 22:03:14 -0300

    ya funciona el pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Tp7Productos-Wilder/'
-})
+  base: '/Tp7Productos-Wilder/'  // 👈 esta línea es necesaria para GitHub Pages
+});
